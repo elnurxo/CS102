c40217c330253f6b0261c9299c71a7871268745d
elnurxo <elnurykh@code.edu.az>
2025-02-06 09:54:36 +0400

    node: server initalize

---
+++ 07. nodejs (server-side)/node-js/script.js
@@ -0,0 +1,16 @@
+console.log("hello node JS");
+
+//BOM, DOM - NODE X
+
+//BOM - window (alert, prompt, confirm, localStorage, sessionStorage, fetch) - Browser
+
+//DOM - document (querySelector, addEventListener, createElement) - HTML
+
+let num1 = 5;
+let num2 = 15;
+let sum = num1 + num2;
+console.log("sum of two numbers is: ", sum);
+
+// window.alert('hey node js');
+
+console.log(document.createElement('a'));
